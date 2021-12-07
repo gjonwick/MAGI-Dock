13df6918c6799d35527cc9bf518cb05d4364af85
gjonwick <jurgenkaftalli@unyt.edu.al>
2021-12-07 09:31:34 +0100

    Added dependencies

---
+++ dependencies.py
@@ -0,0 +1,16 @@
+# Controllers
+from Controllers.BoxAPI import BoxAPI
+from Controllers.JobController import *
+
+# Models
+from Models.ADContext import ADContext
+from Models.Box import Box
+from Models.Ligand import Ligand
+from Models.Receptor import Receptor
+
+# Utils
+from utils.vec3 import *
+from utils.util import *
+
+
+TESTIMPORT = 427
